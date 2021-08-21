c3aec2696a084c5f154dd4f37f2f5d37122a9cbd
Dovganyuk <asdovganyuk@gmail.com>
2021-08-21 16:19:04 +0300

    brought into accordance with pep8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,14 +5,15 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     slug = models.SlugField(max_length=20, unique=True, db_index=True)
-    
+
     def __str__(self):
         return(self.title)
-        
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    
+
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
-    return render(request, template, context) 
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
